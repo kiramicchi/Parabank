1899ead35f5ce9345b39c3c2a82b8b32a575139a
Kiramii <kiramicchi04@gmail.com>
2025-07-24 14:20:22 +0800

    Updated logout.cy.js
    Logout page now utilizes POM file LogoutPage.js

--- cypress/e2e/parabank/logout.cy.js
+++ cypress/e2e/parabank/logout.cy.js
@@ -1,13 +1,16 @@
 /// <reference types="cypress" />
 
+import LogoutPage from '../../pages/LogoutPage';
+
 describe('Parabank Logout Test Case', function () {
   it('Standard Logout', function () {
     cy.fixture('TestData').then(user => {
-      cy.visitParabankAndLogin(user);
+      LogoutPage.visit(); // navigate to the login/register page
+      LogoutPage.login(user); // login with credentials
 
       cy.screenshotWithDate?.('Standard Logout', 'BEFORE');
 
-      cy.logoutFromParabank();
+      LogoutPage.logout(); // perform logout
 
       cy.screenshotWithDate?.('Standard Logout', 'AFTER');
     });
